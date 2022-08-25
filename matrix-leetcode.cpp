28087c94849ab621ef7237741bd57c83c2c489ce
fucking ajeet <ajeet808080@gmail.com>
2022-08-26 00:05:36 +0530

    34th day

--- matrix-leetcode.cpp
+++ matrix-leetcode.cpp
@@ -1,13 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-
-
- bool searchMatrix(vector<vector<int>>& matrix, int target) {
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
        for(int i =0; i<matrix.size(); i++)
        {
-        for(int j =0; j<matrix.size(); j++)
+        for(int j =0; j<matrix[0].size(); j++)
         {
             if(matrix[i][j]==target)
             {
@@ -17,8 +14,11 @@ int main()
        }
        return 0;
     }
+int main()
 {
-
+    int target = 50;
+    vector<vector<int>> mat = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    cout<<searchMatrix(mat,target);
 
 
 

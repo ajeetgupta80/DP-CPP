8729bed0919defa7c0863285e8c9905967eb6696
fucking ajeet <ajeet808080@gmail.com>
2022-08-30 22:53:41 +0530

    38th day

--- nqueens.cpp
+++ nqueens.cpp
@@ -1,11 +1,10 @@
-#include<iostream>
-#include<bits/stdc++.h>
-#include<vector>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-bool ifsafe(int row, int col,vector<string> board, int n)
-{
+class Solution {
+  public:
+    bool isSafe1(int row, int col, vector < string > board, int n) {
+      
     int duprow = row;
     int dupcol = col;
 
@@ -33,38 +32,32 @@ bool ifsafe(int row, int col,vector<string> board, int n)
     while(row<n && col>=0)
     {
         if(board[row][col]=='Q')
-        reutrn false;
-        
+        return false;
+        row++;
+        col--;
 
     }
-
-}
-
-
-
-void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n)
-{
-
-   if(col == n)
-   ans.push_back(board);
-   return;
-
-   for(int row=0; row<n; row++)
-   {
-    if(ifsafe(row,col,board,n))
-    {
-        board[row][col]='Q';
-        solve(col+1,board,ans,n);
-        board[row][col]='.';
+    return true;
     }
 
+  public:
+    void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
+      if (col == n) {
+        ans.push_back(board);
+        return;
+      }
+      for (int row = 0; row < n; row++) {
+        if (isSafe1(row, col, board, n)) {
+          board[row][col] = 'Q';
+          solve(col + 1, board, ans, n);
+          board[row][col] = '.';
+        }
+      }
+    }
 
-   }
-}
-
-vector<vector<string>> solvequeen(int n)
-{
-    vector<string> board(n);
+  public:
+    vector < vector < string >> solveNQueens(int n) {
+      vector<string> board(n);
     vector<vector<string>> ans;
     string s(n,'.');
     for(int i=0; i<n; i++)
@@ -73,19 +66,20 @@ vector<vector<string>> solvequeen(int n)
     }
 
     solve(0,board,ans,n);
-    return ans;
-}
-
-int main()
-{
-
-
-
-
-
-
-
-
-
-
+    return ans;  
+    }
+};
+int main() {
+  int n = 4; // we are taking 4*4 grid and 4 queens
+  Solution obj;
+  vector < vector < string >> ans = obj.solveNQueens(n);
+  for (int i = 0; i < ans.size(); i++) {
+    cout << "Arrangement " << i + 1 << "\n";
+    for (int j = 0; j < ans[0].size(); j++) {
+      cout << ans[i][j];
+      cout << endl;
+    }
+    cout << endl;
+  }
+  return 0;
 }
